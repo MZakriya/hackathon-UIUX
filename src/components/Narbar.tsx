0ecf16d04cb99234d3a5c1d6b9a46a6712084a5c
MZakriya <ziky989@gmail.com>
2024-12-09 23:37:58 +0500

    update

--- src/components/Narbar.tsx
+++ src/components/Narbar.tsx
@@ -80,7 +80,9 @@ const Navbar = () => {
             <FiUser />
             <FiSearch />
             <FiHeart />
+            <Link href={"/components/shopping-cart"}>
             <FiShoppingCart />
+            </Link>
           </div>
         </div>
       )}
